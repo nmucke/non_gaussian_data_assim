ec61a0218e4ec4338a69a6a1f077ec41001ecfd7
nmucke <nmucke@gmail.com>
2026-01-05 16:07:24 -0400

    Add main script for Kuramoto-Sivashinsky model simulation. Implement forward model and data assimilation using Ensemble Kalman Filter. Include visualization of true solution, prior, and posterior ensembles, along with error analysis. Enhance modularity with new forward model class.

--- src/non_gaussian_data_assim/bin/main_kuramoto.py
+++ src/non_gaussian_data_assim/bin/main_kuramoto.py
@@ -12,38 +12,48 @@ from non_gaussian_data_assim.da_methods.enkf import EnsembleKalmanFilter
 # from non_gaussian_data_assim.da_methods.pff import ParticleFlowFilter
 # from non_gaussian_data_assim.da_methods.pff_loc import ParticleFlowFilterLocalization
 from non_gaussian_data_assim.da_methods.pff import ParticleFlowFilter
-from non_gaussian_data_assim.forward_models.lorenz_96 import Lorenz96Model
+from non_gaussian_data_assim.forward_models.kuramoto_sivashinsky import KuramotoSivashinsky
 from non_gaussian_data_assim.observation_operator import ObservationOperator
 
 np.random.seed(42)
 
 # Constants and parameters
-DT = 0.01
-F = 8.0
-NUM_TIME_STEPS = 100
+DT = 0.05
+NU = 1.0
+NUM_TIME_STEPS = 500
 NUM_STATES = 1
-STATE_DIM = 50
-NUM_SKIP = 3
-ENSEMBLE_SIZE = 1000
-
-# Initial state
-X_0 = np.random.randn(NUM_STATES, STATE_DIM) * 10
-X_0[-1] = X_0[0]  # Periodic boundary condition
+STATE_DIM = 1024
+NUM_MODEL_STEPS = 4
+NUM_SKIP_OBS = 4
+ENSEMBLE_SIZE = 100
+DOMAIN_LENGTH = 100
 
 # Observation ids
-OBS_IDS = np.arange(0, STATE_DIM, NUM_SKIP)
+OBS_IDS = np.arange(0, STATE_DIM, NUM_SKIP_OBS)
 OBS_STATES = (0,)
 
 # Observation error covariance matrix
-R = np.eye(len(OBS_IDS)) * 0.1
+R = np.eye(len(OBS_IDS)) * 0.25
+
+x = np.linspace(start=0, stop=DOMAIN_LENGTH, num=STATE_DIM)
+
+# initial condition 
+U0 = lambda magnitude: magnitude * np.cos((2 * np.pi * x) / DOMAIN_LENGTH) + magnitude * np.cos((4 * np.pi * x) / DOMAIN_LENGTH)
+
+U0_PRIOR = lambda magnitude: magnitude * np.cos((2 * np.pi * x) / DOMAIN_LENGTH) + magnitude * np.cos((4 * np.pi * x) / DOMAIN_LENGTH)
+
 
 
 def main() -> None:
     """Main function."""
 
     # Define the forward model
-    forward_model = Lorenz96Model(
-        forcing_term=F, state_dim=STATE_DIM, dt=DT, num_model_steps=1
+    forward_model = KuramotoSivashinsky(
+        dt=DT,
+        num_model_steps=NUM_MODEL_STEPS, 
+        state_dim=STATE_DIM, 
+        domain_length=DOMAIN_LENGTH,
+        nu=NU
     )
 
     # Define the observation operator
@@ -54,34 +64,35 @@ def main() -> None:
     # Initialize the true solution and observations
     observations = np.zeros((len(OBS_IDS), NUM_TIME_STEPS))
     true_sol = np.zeros((1, NUM_STATES, STATE_DIM, NUM_TIME_STEPS))
-    true_sol[0, ..., 0] = X_0
+    true_sol[0, ..., 0] = U0(0.1)
 
     # Perform the forward model
     for i in range(1, NUM_TIME_STEPS):
         true_sol[..., i] = forward_model(true_sol[..., i - 1])
         observations[:, i] = obs_operator(true_sol[..., i])
 
+    # plt.figure()
+    # plt.imshow(true_sol[0, 0, :, -STATE_DIM*2:])
+    # plt.colorbar()
+    # plt.show()
+    # pdb.set_trace()
+
     # Define the data assimilation model
-    da_model = AdaptiveGaussianMixtureFilter(
+    da_model = EnsembleKalmanFilter(
         ensemble_size=ENSEMBLE_SIZE,
         R=R,
         obs_operator=obs_operator,
         forward_operator=forward_model,
-        inflation_factor=1.0,
+        inflation_factor=8.0,
         localization_distance=10,
-        w_prev=np.ones(ENSEMBLE_SIZE) / ENSEMBLE_SIZE,
-        nc_threshold=0.5,
+        # w_prev=np.ones(ENSEMBLE_SIZE) / ENSEMBLE_SIZE,
+        # nc_threshold=0.5,
     )
-    # da_model = ParticleFlowFilter(
-    #     ensemble_size=ENSEMBLE_SIZE,
-    #     R=R,
-    #     obs_operator=obs_operator,
-    #     forward_operator=forward_model,
-    # )
 
     # Initialize the prior ensemble
     prior_ensemble = np.zeros((ENSEMBLE_SIZE, NUM_STATES, STATE_DIM, NUM_TIME_STEPS))
-    prior_ensemble[..., 0] = np.random.randn(ENSEMBLE_SIZE, NUM_STATES, STATE_DIM) * 10
+    magnitude_samples = np.random.uniform(low=0.0, high=1.5, size=ENSEMBLE_SIZE)
+    prior_ensemble[..., 0] = np.array([U0_PRIOR(magnitude) for magnitude in magnitude_samples]).reshape(ENSEMBLE_SIZE, 1, STATE_DIM)
     prior_ensemble[:, :, -1] = prior_ensemble[:, :, 0]
 
     # Initialize the posterior ensemble
@@ -105,31 +116,31 @@ def main() -> None:
 
     true_sol = true_sol[0, 0]
 
-    idx_to_plot = 17
+    idx_to_plot = STATE_DIM//5 + 51
 
     plt.figure()
     plt.subplot(2, 3, 1)
-    plt.imshow(true_sol)
+    plt.imshow(true_sol[..., -STATE_DIM*2:].T, origin="lower")
     plt.colorbar()
     plt.title("True Solution")
     plt.subplot(2, 3, 2)
-    plt.imshow(prior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(prior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM*2:].T, origin="lower")
     plt.colorbar()
     plt.title("Prior Ensemble Mean")
     plt.subplot(2, 3, 3)
-    plt.imshow(posterior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(posterior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM*2:].T, origin="lower")
     plt.colorbar()
     plt.title("Posterior Ensemble Mean")
     plt.subplot(2, 3, 4)
-    plt.imshow(true_sol - prior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(prior_error[..., -STATE_DIM*2:].T, origin="lower")
     plt.colorbar()
     plt.title("|True - Prior| difference")
     plt.subplot(2, 3, 5)
-    plt.imshow(true_sol - posterior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(posterior_error[..., -STATE_DIM*2:].T, origin="lower")
     plt.colorbar()
     plt.title("|True - Posterior| difference")
-    plt.xlabel("Time")
-    plt.ylabel("State")
+    plt.xlabel("Space")
+    plt.ylabel("Time")
     plt.subplot(2, 3, 6)
     plt.plot(
         prior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
@@ -164,8 +175,8 @@ def main() -> None:
         linestyle="--",
     )
     plt.xlabel("Time")
-    plt.ylabel("State 25")
-    plt.ylim(-10, 10)
+    plt.ylabel(f"State {idx_to_plot}")
+    plt.ylim(-3, 3)
     plt.legend()
     plt.show()
 
