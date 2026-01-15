9eb7296b63930521c993ccfa316aba662eb81a5f
nmucke <nmucke@gmail.com>
2026-01-15 17:37:37 -0400

    Refactor Kuramoto and Lorenz '96 models to enhance performance and clarity
    - Reduced the number of outer steps and state dimensions in the Kuramoto model for efficiency.
    - Integrated JAX for random number generation and ensemble initialization in both models.
    - Streamlined the data assimilation process by modifying how prior and posterior ensembles are handled.
    - Improved visualization logic for error analysis and state representation.
    - Enhanced modularity and clarity of the codebase across both scripts.

--- scripts/main_kuramoto.py
+++ scripts/main_kuramoto.py
@@ -24,9 +24,9 @@ np.random.seed(42)
 # Constants and parameters
 DT = 0.05
 NU = 1.0
-OUTER_STEPS = 1500
+OUTER_STEPS = 500
 NUM_STATES = 1
-STATE_DIM = 1024
+STATE_DIM = 512
 INNER_STEPS = 4
 NUM_SKIP_OBS = 4
 ENSEMBLE_SIZE = 100
@@ -49,6 +49,8 @@ X_0_FN = lambda magnitude: magnitude * jnp.cos(
 
 def main() -> None:
     """Main function."""
+
+    rng_key = jax.random.PRNGKey(42)
 
     # Define the forward model
     forward_model = KuramotoSivashinsky(
@@ -82,80 +84,85 @@ def main() -> None:
     )
 
     # Initialize the prior ensemble
-    prior_ensemble = np.zeros((ENSEMBLE_SIZE, NUM_STATES, STATE_DIM, OUTER_STEPS))
-    magnitude_samples = np.random.uniform(low=0.0, high=1.5, size=ENSEMBLE_SIZE)
-    prior_ensemble[..., 0] = np.array(
-        [X_0_FN(magnitude) for magnitude in magnitude_samples]
-    ).reshape(ENSEMBLE_SIZE, 1, STATE_DIM)
-    prior_ensemble[:, :, -1] = prior_ensemble[:, :, 0]
+    rng_key, key = jax.random.split(rng_key)
+    magnitude_samples = jax.random.uniform(
+        key, (ENSEMBLE_SIZE,), minval=0.0, maxval=1.5
+    )
+    prior_ensemble = jnp.array([X_0_FN(magnitude) for magnitude in magnitude_samples])
+    prior_ensemble = prior_ensemble.reshape(ENSEMBLE_SIZE, NUM_STATES, STATE_DIM)
 
     # Initialize the posterior ensemble
     posterior_ensemble = prior_ensemble.copy()
+    posterior_ensemble = posterior_ensemble.reshape(
+        ENSEMBLE_SIZE, 1, NUM_STATES, STATE_DIM
+    )
+
+    # Rollout the prior ensemble
+    prior_ensemble = forward_model.rollout(prior_ensemble, OUTER_STEPS - 1)
 
     # Perform the data assimilation
+    t = 0.0
     for i in tqdm(range(1, OUTER_STEPS)):
-        prior_ensemble[..., i] = forward_model(prior_ensemble[..., i - 1])
-        posterior_ensemble[..., i] = da_model(
-            prior_ensemble=posterior_ensemble[..., i - 1], obs_vect=observations[:, i]
+        posterior_next = da_model(
+            prior_ensemble=posterior_ensemble[:, i - 1], obs_vect=observations[:, i]
+        )
+        posterior_ensemble = jnp.concatenate(
+            [posterior_ensemble, posterior_next[:, None, :, :]], axis=1
         )
 
+    true_sol = true_sol.reshape(OUTER_STEPS, STATE_DIM)
+
     # Calculate the prior and posterior errors
-    prior_error = true_sol - prior_ensemble.mean(axis=(0, 1))
-    prior_error = np.sqrt(np.sum(prior_error**2, axis=(0, 1)))
-    posterior_error = true_sol - posterior_ensemble.mean(axis=(0, 1))
-    posterior_error = np.sqrt(np.sum(posterior_error**2, axis=(0, 1)))
+    prior_error = true_sol - prior_ensemble.mean(axis=(0, 2))
+    prior_error = np.sqrt(np.sum(prior_error**2))
+    posterior_error = true_sol - posterior_ensemble.mean(axis=(0, 2))
+    posterior_error = np.sqrt(np.sum(posterior_error**2))
 
-    print(f"Prior error: {prior_error.mean()}")
-    print(f"Posterior error: {posterior_error.mean()}")
+    print(f"Prior error: {prior_error}")
+    print(f"Posterior error: {posterior_error}")
 
-    true_sol = true_sol[0, 0]
-
-    idx_to_plot = STATE_DIM // 5 + 51
+    idx_to_plot = 17
 
     plt.figure()
     plt.subplot(2, 3, 1)
-    plt.imshow(true_sol[..., -STATE_DIM * 2 :].T, origin="lower")
+    plt.imshow(true_sol)
     plt.colorbar()
     plt.title("True Solution")
     plt.subplot(2, 3, 2)
-    plt.imshow(
-        prior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM * 2 :].T, origin="lower"
-    )
+    plt.imshow(prior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("Prior Ensemble Mean")
     plt.subplot(2, 3, 3)
-    plt.imshow(
-        posterior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM * 2 :].T, origin="lower"
-    )
+    plt.imshow(posterior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("Posterior Ensemble Mean")
     plt.subplot(2, 3, 4)
-    plt.imshow(prior_error[..., -STATE_DIM * 2 :].T, origin="lower")
+    plt.imshow(true_sol - prior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("|True - Prior| difference")
     plt.subplot(2, 3, 5)
-    plt.imshow(posterior_error[..., -STATE_DIM * 2 :].T, origin="lower")
+    plt.imshow(true_sol - posterior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("|True - Posterior| difference")
-    plt.xlabel("Space")
-    plt.ylabel("Time")
+    plt.xlabel("Time")
+    plt.ylabel("State")
     plt.subplot(2, 3, 6)
     plt.plot(
-        prior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+        prior_ensemble.mean(axis=(0, 2))[:, idx_to_plot],
         label="Prior",
         color="tab:red",
         linewidth=3,
     )
     plt.plot(
-        posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+        posterior_ensemble.mean(axis=(0, 2))[:, idx_to_plot],
         label="Posterior",
         color="tab:blue",
         linewidth=3,
     )
     # Shade the standard deviation of the posterior on the time series plot
-    mean_post = posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1))
-    std_post = posterior_ensemble[:, :, idx_to_plot, :].std(axis=(0, 1))
-    time_axis = np.arange(posterior_ensemble.shape[-1])
+    mean_post = posterior_ensemble.mean(axis=(0, 2))[:, idx_to_plot]
+    std_post = posterior_ensemble.std(axis=(0, 2))[:, idx_to_plot]
+    time_axis = np.arange(posterior_ensemble.shape[1])
     plt.fill_between(
         time_axis,
         mean_post - std_post,
@@ -166,17 +173,114 @@ def main() -> None:
     )
 
     plt.plot(
-        true_sol[idx_to_plot, :],
+        true_sol[:, idx_to_plot],
         label="True",
         color="black",
         linewidth=3,
         linestyle="--",
     )
     plt.xlabel("Time")
-    plt.ylabel(f"State {idx_to_plot}")
-    plt.ylim(-3, 3)
+    plt.ylabel("State 25")
+    plt.ylim(-10, 10)
     plt.legend()
     plt.show()
+
+    # # Initialize the prior ensemble
+    # prior_ensemble = np.zeros((ENSEMBLE_SIZE, NUM_STATES, STATE_DIM, OUTER_STEPS))
+    # magnitude_samples = np.random.uniform(low=0.0, high=1.5, size=ENSEMBLE_SIZE)
+    # prior_ensemble[..., 0] = np.array(
+    #     [X_0_FN(magnitude) for magnitude in magnitude_samples]
+    # ).reshape(ENSEMBLE_SIZE, 1, STATE_DIM)
+    # prior_ensemble[:, :, -1] = prior_ensemble[:, :, 0]
+
+    # # Initialize the posterior ensemble
+    # posterior_ensemble = prior_ensemble.copy()
+
+    # # Perform the data assimilation
+    # for i in tqdm(range(1, OUTER_STEPS)):
+    #     prior_ensemble[..., i] = forward_model(prior_ensemble[..., i - 1])
+    #     posterior_ensemble[..., i] = da_model(
+    #         prior_ensemble=posterior_ensemble[..., i - 1], obs_vect=observations[:, i]
+    #     )
+
+    # # Calculate the prior and posterior errors
+    # prior_error = true_sol - prior_ensemble.mean(axis=(0, 1))
+    # prior_error = np.sqrt(np.sum(prior_error**2, axis=(0, 1)))
+    # posterior_error = true_sol - posterior_ensemble.mean(axis=(0, 1))
+    # posterior_error = np.sqrt(np.sum(posterior_error**2, axis=(0, 1)))
+
+    # print(f"Prior error: {prior_error.mean()}")
+    # print(f"Posterior error: {posterior_error.mean()}")
+
+    # true_sol = true_sol[0, 0]
+
+    # idx_to_plot = STATE_DIM // 5 + 51
+
+    # plt.figure()
+    # plt.subplot(2, 3, 1)
+    # plt.imshow(true_sol[..., -STATE_DIM * 2 :].T, origin="lower")
+    # plt.colorbar()
+    # plt.title("True Solution")
+    # plt.subplot(2, 3, 2)
+    # plt.imshow(
+    #     prior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM * 2 :].T, origin="lower"
+    # )
+    # plt.colorbar()
+    # plt.title("Prior Ensemble Mean")
+    # plt.subplot(2, 3, 3)
+    # plt.imshow(
+    #     posterior_ensemble.mean(axis=(0, 1))[..., -STATE_DIM * 2 :].T, origin="lower"
+    # )
+    # plt.colorbar()
+    # plt.title("Posterior Ensemble Mean")
+    # plt.subplot(2, 3, 4)
+    # plt.imshow(prior_error[..., -STATE_DIM * 2 :].T, origin="lower")
+    # plt.colorbar()
+    # plt.title("|True - Prior| difference")
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(posterior_error[..., -STATE_DIM * 2 :].T, origin="lower")
+    # plt.colorbar()
+    # plt.title("|True - Posterior| difference")
+    # plt.xlabel("Space")
+    # plt.ylabel("Time")
+    # plt.subplot(2, 3, 6)
+    # plt.plot(
+    #     prior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+    #     label="Prior",
+    #     color="tab:red",
+    #     linewidth=3,
+    # )
+    # plt.plot(
+    #     posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+    #     label="Posterior",
+    #     color="tab:blue",
+    #     linewidth=3,
+    # )
+    # # Shade the standard deviation of the posterior on the time series plot
+    # mean_post = posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1))
+    # std_post = posterior_ensemble[:, :, idx_to_plot, :].std(axis=(0, 1))
+    # time_axis = np.arange(posterior_ensemble.shape[-1])
+    # plt.fill_between(
+    #     time_axis,
+    #     mean_post - std_post,
+    #     mean_post + std_post,
+    #     color="tab:blue",
+    #     alpha=0.2,
+    #     label="Posterior ± Std",
+    # )
+
+    # plt.plot(
+    #     true_sol[idx_to_plot, :],
+    #     label="True",
+    #     color="black",
+    #     linewidth=3,
+    #     linestyle="--",
+    # )
+    # plt.xlabel("Time")
+    # plt.ylabel(f"State {idx_to_plot}")
+    # plt.ylim(-3, 3)
+    # plt.legend()
+    # plt.show()
 
 
 if __name__ == "__main__":
--- scripts/main_lorenz_96.py
+++ scripts/main_lorenz_96.py
@@ -20,7 +20,7 @@ DT = 0.01
 F = 8.0
 T0 = 0.0
 
-OUTER_STEPS = 100
+OUTER_STEPS = 150
 INNER_STEPS = 4
 
 
@@ -28,13 +28,6 @@ NUM_STATES = 1
 STATE_DIM = 25
 NUM_SKIP = 3
 ENSEMBLE_SIZE = 100
-
-rng_key = jax.random.PRNGKey(SEED)
-
-# Initial state - shape: [1, num_states, state_dim] for single ensemble member
-rng_key, key = jax.random.split(rng_key)
-X_0 = jax.random.normal(key, (1, NUM_STATES, STATE_DIM)) * 10
-X_0 = X_0.at[0, 0, -1].set(X_0[0, 0, 0])  # Periodic boundary condition
 
 # Observation ids
 OBS_IDS = np.arange(0, STATE_DIM, NUM_SKIP)
@@ -46,6 +39,12 @@ R = jnp.eye(len(OBS_IDS)) * 0.1
 
 def main() -> None:
     """Main function."""
+    rng_key = jax.random.PRNGKey(SEED)
+
+    # Initial state - shape: [1, num_states, state_dim] for single ensemble member
+    rng_key, key = jax.random.split(rng_key)
+    X_0 = jax.random.normal(key, (1, NUM_STATES, STATE_DIM)) * 10
+    X_0 = X_0.at[0, 0, -1].set(X_0[0, 0, 0])  # Periodic boundary condition
 
     # Define the forward model
     forward_model = Lorenz96Model(
@@ -85,31 +84,42 @@ def main() -> None:
     # )
 
     # Initialize the prior ensemble
-    prior_ensemble = np.zeros((ENSEMBLE_SIZE, NUM_STATES, STATE_DIM, OUTER_STEPS))
-    prior_ensemble[..., 0] = np.random.randn(ENSEMBLE_SIZE, NUM_STATES, STATE_DIM) * 10
-    prior_ensemble[:, :, -1] = prior_ensemble[:, :, 0]
+    rng_key, key = jax.random.split(rng_key)
+    prior_ensemble = jax.random.normal(key, (ENSEMBLE_SIZE, NUM_STATES, STATE_DIM)) * 10
+    prior_ensemble = prior_ensemble.at[:, :, -1].set(
+        prior_ensemble[:, :, 0]
+    )  # Periodic boundary condition
 
     # Initialize the posterior ensemble
     posterior_ensemble = prior_ensemble.copy()
+    posterior_ensemble = posterior_ensemble.reshape(
+        ENSEMBLE_SIZE, 1, NUM_STATES, STATE_DIM
+    )
+
+    # Rollout the prior ensemble
+    prior_ensemble = forward_model.rollout(prior_ensemble, OUTER_STEPS - 1)
 
     # Perform the data assimilation
     t = T0
     for i in tqdm(range(1, OUTER_STEPS)):
-        prior_ensemble[..., i] = forward_model(prior_ensemble[..., i - 1])
-        posterior_ensemble[..., i] = da_model(
-            prior_ensemble=posterior_ensemble[..., i - 1], obs_vect=observations[:, i]
+        posterior_next = da_model(
+            prior_ensemble=posterior_ensemble[:, i - 1], obs_vect=observations[:, i]
+        )
+        posterior_ensemble = jnp.concatenate(
+            [posterior_ensemble, posterior_next[:, None, :, :]], axis=1
         )
 
     # Calculate the prior and posterior errors
-    prior_error = true_sol - prior_ensemble.mean(axis=(0, 1))
-    prior_error = np.sqrt(np.sum(prior_error**2, axis=(0, 1)))
-    posterior_error = true_sol - posterior_ensemble.mean(axis=(0, 1))
-    posterior_error = np.sqrt(np.sum(posterior_error**2, axis=(0, 1)))
 
-    print(f"Prior error: {prior_error.mean()}")
-    print(f"Posterior error: {posterior_error.mean()}")
+    true_sol = true_sol.reshape(OUTER_STEPS, STATE_DIM)
 
-    true_sol = true_sol[0, 0]
+    prior_error = true_sol - prior_ensemble.mean(axis=(0, 2))
+    prior_error = np.sqrt(np.sum(prior_error**2))
+    posterior_error = true_sol - posterior_ensemble.mean(axis=(0, 2))
+    posterior_error = np.sqrt(np.sum(posterior_error**2))
+
+    print(f"Prior error: {prior_error}")
+    print(f"Posterior error: {posterior_error}")
 
     idx_to_plot = 17
 
@@ -119,40 +129,40 @@ def main() -> None:
     plt.colorbar()
     plt.title("True Solution")
     plt.subplot(2, 3, 2)
-    plt.imshow(prior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(prior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("Prior Ensemble Mean")
     plt.subplot(2, 3, 3)
-    plt.imshow(posterior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(posterior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("Posterior Ensemble Mean")
     plt.subplot(2, 3, 4)
-    plt.imshow(true_sol - prior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(true_sol - prior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("|True - Prior| difference")
     plt.subplot(2, 3, 5)
-    plt.imshow(true_sol - posterior_ensemble.mean(axis=(0, 1)))
+    plt.imshow(true_sol - posterior_ensemble.mean(axis=(0, 2)))
     plt.colorbar()
     plt.title("|True - Posterior| difference")
     plt.xlabel("Time")
     plt.ylabel("State")
     plt.subplot(2, 3, 6)
     plt.plot(
-        prior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+        prior_ensemble.mean(axis=(0, 2))[:, idx_to_plot],
         label="Prior",
         color="tab:red",
         linewidth=3,
     )
     plt.plot(
-        posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1)),
+        posterior_ensemble.mean(axis=(0, 2))[:, idx_to_plot],
         label="Posterior",
         color="tab:blue",
         linewidth=3,
     )
     # Shade the standard deviation of the posterior on the time series plot
-    mean_post = posterior_ensemble[:, :, idx_to_plot, :].mean(axis=(0, 1))
-    std_post = posterior_ensemble[:, :, idx_to_plot, :].std(axis=(0, 1))
-    time_axis = np.arange(posterior_ensemble.shape[-1])
+    mean_post = posterior_ensemble.mean(axis=(0, 2))[:, idx_to_plot]
+    std_post = posterior_ensemble.std(axis=(0, 2))[:, idx_to_plot]
+    time_axis = np.arange(posterior_ensemble.shape[1])
     plt.fill_between(
         time_axis,
         mean_post - std_post,
@@ -163,7 +173,7 @@ def main() -> None:
     )
 
     plt.plot(
-        true_sol[idx_to_plot, :],
+        true_sol[:, idx_to_plot],
         label="True",
         color="black",
         linewidth=3,
